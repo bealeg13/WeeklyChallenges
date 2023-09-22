68fee2707dabecc1a89ac8a3a749675f56822713
Gabriel Bealer <bealeg13@gmail.com>
2023-09-21 20:05:10 -0700

    finished weekly challenge set 4

--- ChallengesWithTestsMark8/ChallengesSet04.cs
+++ ChallengesWithTestsMark8/ChallengesSet04.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace ChallengesWithTestsMark8
 {
@@ -6,47 +8,141 @@ namespace ChallengesWithTestsMark8
     {
         public int AddEvenSubtractOdd(int[] numbers)
         {
-            throw new NotImplementedException();
+            int result = 0;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                {
+                    result += numbers[i];
+                }
+                else 
+                {
+                    result -= numbers[i];
+                }
+            }
+            return result;
+
         }
 
         public int GetLengthOfShortestString(string str1, string str2, string str3, string str4)
         {
-            throw new NotImplementedException();
+            string[] stringArray = { str1, str2, str3, str4 };
+            string shortestString = stringArray.OrderBy(x => x.Length).FirstOrDefault();
+            
+            if (shortestString == null)
+            {
+                return 0;
+            }
+            return shortestString.Length;
+            
+
+           
         }
 
         public int GetSmallestNumber(int number1, int number2, int number3, int number4)
         {
-            throw new NotImplementedException();
+            int[] numberArray = { number1, number2, number3, number4 };
+            int smallestNumber = numberArray.OrderBy(x => x).FirstOrDefault();
+            return smallestNumber;
         }
 
         public void ChangeBusinessNameTo_TrueCoders(Business biz)
         {
-            throw new NotImplementedException();
+            biz.Name = "TrueCoders";
+            Console.WriteLine(biz.Name);
         }
 
         public bool CouldFormTriangle(int sideLength1, int sideLength2, int sideLength3)
         {
-            throw new NotImplementedException();
+           if (sideLength1 + sideLength2 > sideLength3 && sideLength2 + sideLength3 > sideLength1 && sideLength3 + sideLength1 > sideLength2) 
+            {
+                return true;
+            }
+           return false;    
+          
         }
 
         public bool IsStringANumber(string input)
         {
-            throw new NotImplementedException();
+            //char[] chars = input.ToCharArray();
+            //foreach (char c in chars) 
+            //{
+            //    if (char.)
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return true;
+
+            //return !string.IsNullOrWhiteSpace(input) && input.All(char.IsDigit);
+            if (string.IsNullOrEmpty(input))
+            {
+                return false;
+            }
+            if (double.TryParse(input, out _))
+            {
+                return true;
+            }
+            return false;
+
+            
         }
 
         public bool MajorityOfElementsInArrayAreNull(object[] objs)
         {
-            throw new NotImplementedException();
+            int nullValues = 0;
+            int noNullValues = 0;
+            for (int i = 0; i < objs.Length; i++)
+            {
+                if (objs[i] == null)
+                {
+                    nullValues++;
+                }
+                else
+                {
+                    noNullValues++;
+                }
+
+            }
+            return nullValues > noNullValues;
+            
         }
 
         public double AverageEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null || !numbers.Any())
+            {
+                return 0;
+            }
+
+            double result = 0;
+            int count = 0;
+            foreach (int number in numbers)
+            {
+                if (number % 2 == 0)
+                {
+                    result += number;
+                    count++;
+                }
+                
+            }
+            if (count == 0)
+            {
+                return 0;
+            }
+            return result / count; 
         }
 
         public int Factorial(int number)
         {
-            throw new NotImplementedException();
+           if (number <= 1)
+            {
+                return 1;
+            }
+           else
+            {
+                return number * Factorial(number -1);
+            }
         }
     }
 }
